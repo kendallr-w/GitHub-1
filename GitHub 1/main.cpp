dbf6580dfe11d57f3e6797bc1a4c27e780e43e80
Kendall Reed-Warren <reedwarrenkj@gmail.com>
2025-08-28 09:33:41 -0500

    i added a fantastic sum

--- GitHub 1/main.cpp
+++ GitHub 1/main.cpp
@@ -2,5 +2,9 @@
 
 int main()
 {
-	std::cout << "rwererwtwrtwtw";
+	std::cout << "hello world\n";// version control
+
+	std::cout << "this is my other sentence.\n";// version control
+
+	//what does \n do? It moves the cursor to the NEW line
 }
