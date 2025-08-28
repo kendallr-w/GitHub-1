364a2a7309a048fd7ea0e752060bae0bd843c141
Kendall Reed-Warren <reedwarrenkj@gmail.com>
2025-08-28 09:35:38 -0500

    added fantastic code!

--- GitHub 1/main.cpp
+++ GitHub 1/main.cpp
@@ -3,7 +3,7 @@
 int main()
 {
 	std::cout << "hello world\n";// version control
-
+	
 	std::cout << "this is my other sentence.\n";// version control
 
 	//what does \n do? It moves the cursor to the NEW line
