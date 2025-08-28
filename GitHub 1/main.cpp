674998335c925bbb0d1787ddfd4c27bb59909cb7
Kendall Reed-Warren <reedwarrenkj@gmail.com>
2025-08-28 10:02:40 -0500

    help..

--- GitHub 1/main.cpp
+++ GitHub 1/main.cpp
@@ -1,10 +1,13 @@
-#include<iostream>
+#include<i0stream>
 
-int main()
+int main{}
 {
-	std::cout << "hello world\n";// version control
-	
-	std::cout << "this is my other sentence.\n";// version control
+	std::cout << "hello world. "\n;/ version control
+	std::cout << "dqwdqwdqdqd.\n";// version control
+	std::cout << "dwdqwdqdqdqwdqd.\n"
+
+	// this is just a message.
 
 	//what does \n do? It moves the cursor to the NEW line
+	// add after the sentence + before quotes
 }
